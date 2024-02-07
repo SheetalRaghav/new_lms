0af8bb8d501b0bec44a9a2562bb2fe16c9fea299
SheetalRaghav <135951161+SheetalRaghav@users.noreply.github.com>
2024-02-07 18:28:55 +0530

    course selection added

--- New-LMS/src/routes/router.jsx
+++ New-LMS/src/routes/router.jsx
@@ -1,6 +1,6 @@
 import {
-    createBrowserRouter,
-  } from "react-router-dom";
+  createBrowserRouter,
+} from "react-router-dom";
 import React from 'react'
 import Register from '../pages/Register'
 import Login from "../pages/Login";
@@ -12,50 +12,54 @@ import AddCourse from "../components/Common/AddCourse";
 import CategoryPage from "../components/Admin/CategoryPage";
 import SelectCourseForCurriculum from "../components/Common/SelectCourseForCurriculum";
 import AddCurriculum from "../components/Common/AddCurriculum";
-  const router = createBrowserRouter([
-    {
-        path:'/login',
-        element:<Login></Login>
-    },
-    {
-      path: "/",
-      element: <Main/>,
-      children: [
-        {
-          path: "/",
-          element: <Dashboard/>,
-        },
-        {
-          path: "register",
-          element: <Register></Register>,
-        },
-        {
-          path: "users",
-          element: <ManageUser></ManageUser>,
-        },
-        {
-          path:"courses",
-          element:<AllCourses></AllCourses>
-        },
-        {
-          path:"new-course",
-          element:<AddCourse/>
-        },
-        {
-          path:"category",
-          element:<CategoryPage/>
-        },
-        {
-          path:"curriculum",
-          element:<SelectCourseForCurriculum></SelectCourseForCurriculum>,
-        },
-        {
-          path:"curriculum/:id",
-          element:<AddCurriculum></AddCurriculum>,
-        },
+import CourseAssign from "../components/Admin/CourseAssign";
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <Login></Login>
+  },
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+      {
+        path: "users",
+        element: <ManageUser></ManageUser>,
+      },
+      {
+        path: "courses",
+        element: <AllCourses></AllCourses>
+      },
+      {
+        path: "new-course",
+        element: <AddCourse />
+      },
+      {
+        path: "category",
+        element: <CategoryPage />
+      },
+      {
+        path: "curriculum",
+        element: <SelectCourseForCurriculum></SelectCourseForCurriculum>,
+      },
+      {
+        path: "curriculum/:id",
+        element: <AddCurriculum></AddCurriculum>,
+      },
+      {
+        path: 'student-course',
+        element: <CourseAssign />
+      }
 
-        
-      ],
-    }
-  ]);
-  export default router
+    ],
+  }
+]);
+export default router
